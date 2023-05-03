ba4b0bae7732c9583048561527883e7e2cb2f2bd
ihunterghost <wendello@dcomp.ufs.br>
2023-05-03 16:05:54 -0300

    update-len_field

--- core/models.py
+++ core/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class log(models.Model):
     mac = models.CharField(max_length=50)
-    slave = models.CharField(max_length=10)
+    slave = models.CharField(max_length=50)
     message = models.CharField(max_length=100)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
